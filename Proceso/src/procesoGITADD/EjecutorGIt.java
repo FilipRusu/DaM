886e3a2d3e3edb3a005664fd8996e87bfd29223a
Filip <cobaiul4666@gmail.com>
2025-09-30 17:05:56 +0200

    "$(date +%x)"

---
+++ Proceso/src/procesoGITADD/EjecutorGIt.java
@@ -0,0 +1,40 @@
+package procesoGITADD;
+
+import java.io.File;
+
+public class EjecutorGIt {
+	
+		public static void subiraGITHUB()	throws Exception {
+		File f = new File("../");
+		ProcessBuilder pb = new ProcessBuilder("git","add",".");
+		pb.directory(f);
+		
+		ProcessBuilder pb1 = new ProcessBuilder("git","commit","-m","\"$(date +%x)\"");
+		pb.directory(f);
+		
+		ProcessBuilder pb2 = new ProcessBuilder("git","push","-u","origin","master");
+		pb.directory(f);
+		
+		
+		Process procesoadd=pb.start();
+		Process procesocommit=pb1.start();
+		Process procesopush=pb2.start();
+		
+		
+		}
+		public static void gitCommit() throws Exception{
+
+			
+		}	
+	
+	
+	
+	public static void main(String[] args) {
+			try {
+				subiraGITHUB();
+			} catch (Exception e) {
+			
+			}
+	}
+	
+}
