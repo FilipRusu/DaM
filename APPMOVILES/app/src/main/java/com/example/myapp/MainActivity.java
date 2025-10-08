bdfbb09530743bafc2d83182fd8f73318af6d3dd
Filip <cobaiul4666@gmail.com>
2025-10-08 15:54:27 +0200

    08/10/25

--- APPMOVILES/app/src/main/java/com/example/myapp/MainActivity.java
+++ APPMOVILES/app/src/main/java/com/example/myapp/MainActivity.java
@@ -1,20 +1,38 @@
 package com.example.myapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText txtNombre;
+    private Button btnAceptar;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Obtenemos una referencia a los controles de la interfaz
+        txtNombre = findViewById(R.id.txtnombre);
+        btnAceptar = findViewById(R.id.btnAcceptar);
 
+        btnAceptar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Creamos el Intent
+                Intent intent = new Intent(MainActivity.this, SaludoActivity.class);
+                // Añadimos al intent la información a pasar entre actividades
+                intent.putExtra("NOMBRE", txtNombre.getText().toString());
+                // Iniciamos la nueva actividad
+                startActivity(intent);
+            }
+        });
     }
 }
--- APPMOVILES/app/src/main/java/com/example/myapp/SaludoActivity.java
+++ APPMOVILES/app/src/main/java/com/example/myapp/SaludoActivity.java
@@ -1,18 +1,25 @@
  package com.example.myapp;
 
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
+ import android.os.Bundle;
+ import android.widget.TextView;
+ import androidx.appcompat.app.AppCompatActivity;
 
  public class SaludoActivity extends AppCompatActivity {
+
+     private TextView txtSaludo;
+
      @Override
      protected void onCreate(Bundle savedInstanceState) {
          super.onCreate(savedInstanceState);
          setContentView(R.layout.activity_saludo);
+
+         // Obtenemos una referencia a los controles de la interfaz
+         txtSaludo = findViewById(R.id.txtSaludo);
+
+         // Recuperamos la información pasada en el intent
+         String saludo = getIntent().getStringExtra("NOMBRE");
+
+         // Construimos el mensaje a mostrar
+         txtSaludo.setText("Hola " + saludo);
      }
  }
